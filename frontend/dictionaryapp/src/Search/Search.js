2fffeeaff4ba51a670082c3573395c1c70c0a1bf
zenraiching <rickdavidsangalang@gmail.com>
2021-09-03 00:42:14 +0800

    function test

--- frontend/dictionaryapp/src/Search/Search.js
+++ frontend/dictionaryapp/src/Search/Search.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function SearchComponent(){
+    const sayHello = (name) => {
+        alert("Hello!"+name);
+      };
     return(
         <div className="searchBox">
             <input type="text"></input>
+            <button onClick={sayHello("df")}>Search</button>
         </div>
     );
 }
