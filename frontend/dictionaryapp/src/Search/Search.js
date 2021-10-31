08f4519bec1895bb616c32b393c86ada01fea3e5
raiiiz <rickdavidsangalang@gmail.com>
2021-11-01 01:55:06 +0800

    dfkdfd

--- frontend/dictionaryapp/src/Search/Search.js
+++ frontend/dictionaryapp/src/Search/Search.js
@@ -3,11 +3,13 @@ import React from 'react';
 function SearchComponent(){
     const inputTextChange = (e) => {
         console.log(e.target.value);
+
     }
     return(
         <div className="searchBox">
             <input onChange={inputTextChange} type="text"></input>
             <button>Search</button>
+            <div>Hi</div>
         </div>
     );
 }
